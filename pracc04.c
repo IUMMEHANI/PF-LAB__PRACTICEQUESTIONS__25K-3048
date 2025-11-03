2361abe497de5e6a810b5e4137abe48c140da4b6
UMM E HANI <k253048@nu.edu.pk>
2025-11-03 12:09:34 +0500

    Add files via upload

---
+++ pracc04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+char str[100];
+int i=0;
+printf("Enter a sentence:\n");
+scanf("%[^\n]",str);
+while(str[i]!='\0'){
+if(str[i]>='A'&&str[i]<='Z')str[i]=str[i]+32;
+else if(str[i]>='a'&&str[i]<='z')str[i]=str[i]-32;
+i++;
+}
+printf("\nConverted message:\n%s\n",str);
+return 0;
+}
